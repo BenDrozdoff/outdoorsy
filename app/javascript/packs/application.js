b47dc9faec0a2fb5702921f6d94c9d19a0dc3ee2
Ben Drozdoff <bendrozdoff@gmail.com>
2022-04-23 22:49:13 -0400

    Add styling via bootstrap
    No functional changes in this commit, but it makes it a whole lot easier to look at :)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,3 +8,23 @@ import * as ActiveStorage from "@rails/activestorage"
 
 Rails.start()
 ActiveStorage.start()
+
+require("@popperjs/core")
+
+import "bootstrap"
+
+import { Tooltip, Popover } from "bootstrap"
+
+require("../stylesheets/application.scss")
+
+jQuery(function () {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+      return new Tooltip(tooltipTriggerEl)
+  })
+
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+      return new Popover(popoverTriggerEl)
+  })
+})
